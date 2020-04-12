d15df384ff014e5ac4561093eda635f707c0ce76
Nina Mitevska <nina.mitevska@gmail.com>
2020-04-12 14:13:48 +0200

    grades list added

--- Student.java
+++ Student.java
@@ -2,16 +2,16 @@ class Student {
 	String index;
 	String firstName;
 	String lastName;
+	List<Integer> grades;
 
-	//TODO constructor
 
-	//TODO seters & getters
-
+	
 	public double getAverage() {
 		//TODO
 	}
 
 	public int ECTSCredits() {
 		//TODO
+
 	}
 }
